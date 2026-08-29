1f4014dbb034fd4308dc0e6c58689c8e8db243cd
Weslley Murdock <weslleyluiz@outlook.com.br>
2026-08-29 05:30:38 -0300

    feat: add new method to theme service to retrieve the desired theme

--- src/app/MemoAna/Game/Services/ThemeService.cs
+++ src/app/MemoAna/Game/Services/ThemeService.cs
@@ -10,4 +10,10 @@ public sealed class ThemeService(IRepository<CardThemeManifestEntity> repository
     public async Task<IReadOnlyCollection<CardThemeManifestDto>> GetThemesAsync()
         => [.. (await repository.ListAsync(x => true, null!, CancellationToken.None))
             .Select(theme => new CardThemeManifestDto(theme) )];
+
+    public async Task<CardThemeDto> GetThemeAsync(string themeName)
+    {
+        var manifest = await repository.FirstOrDefaultAsync(x => x.ThemeName == themeName, [x => x.CardTheme], CancellationToken.None);
+        return manifest is null ? throw new InvalidOperationException($"Theme '{themeName}' not found.") : new CardThemeDto(manifest.CardTheme!.Base64Images, manifest.Id, new(manifest));
+    }
 }
